bdd5c6743eecf9fc87b4edb48194f73770df8241
zxy <z17706015970@163.com>
2022-02-20 21:52:24 +0800

    project init

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+  import HelloWorld from './components/HelloWorld.vue'
+  import TestPropsEmit from './components/test-props-emit/index.vue';
+  import { ref, onMounted} from 'vue';
+  const msg = ref('hello world')
+  const handleChange = (params:string)=>{
+    console.log('params',params)
+  }
+  const propsEmitRef = ref()
+  onMounted(()=>{
+    console.log('value',propsEmitRef.value)
+  })
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <TestPropsEmit ref="propsEmitRef" :msg="msg" @on-change="handleChange"></TestPropsEmit>
+  <!-- <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" /> -->
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/test-props-emit/index.vue
@@ -0,0 +1,23 @@
+<template>
+    <p>{{props.msg}}</p>
+    <button @click="handleClick">点我</button>
+</template>
+<script  setup lang="ts">
+    const props = defineProps({
+        msg:{
+            type:String,
+            default:()=>'默认值'
+        }
+    })
+    const emit = defineEmits(['on-change'])
+    const handleClick = ()=>{
+        emit('on-change','父组件方法被调用了')
+    }
+    const childNode = ()=>{
+        console.log('子组件方法被调用了')
+    }
+    defineExpose({
+        demo:'我是dmeo',
+        child: '我是暴露的子组件'
+    })
+</script>
